37e824c0723038b83489cb29321f061eb6a1206a
tomo <sunnyday20200101@gmail.com>
2026-04-07 22:21:49 +0900

    カテゴリ

--- src/services/sheet.ts
+++ src/services/sheet.ts
@@ -1,6 +1,10 @@
 import { google } from "googleapis";
 import * as path from "path";
-
+type CategoryRule = {
+  keyword: string;
+  main: string;
+  sub: string;
+};
 export function createSheetsClient(): any {
   const serviceAccountKeyJson = process.env.GOOGLE_SERVICE_ACCOUNT_KEY;
 
@@ -41,18 +45,44 @@ export async function writeSheetData(
     .filter((row) => row[0] !== "No.")
     .map((row) => row[0]);
 
-
   const filteredData = data.filter((row) => {
     const rowString = row[0];
     return !existingIds.includes(rowString);
   });
 
+  const rules: CategoryRule[] = JSON.parse(process.env.CATEGORY_RULES || "[]");
+
+  const result = filteredData.map((row) => {
+    const store = row[3]; // 利用先
+
+    const { main, sub } = categorize(store, rules);
+    return [row[0], row[1], row[2], row[3], main, sub];
+  });
   await sheets.spreadsheets.values.append({
     spreadsheetId,
     range: `${sheetName}!A3`,
     valueInputOption: "RAW",
     requestBody: {
-      values: filteredData,
+      values: result,
     },
   });
+}
+
+function normalize(str: string) {
+  return str
+    .toUpperCase()
+    .replace(/[Ａ-Ｚａ-ｚ０-９]/g, (s) =>
+      String.fromCharCode(s.charCodeAt(0) - 0xfee0),
+    );
+}
+function categorize(store: string, rules: CategoryRule[]) {
+  const normalized = normalize(store);
+
+  const hit = rules.find((rule) =>
+    normalized.includes(normalize(rule.keyword)),
+  );
+
+  return hit
+    ? { main: hit.main, sub: hit.sub }
+    : { main: "未分類", sub: "未分類" };
 }
